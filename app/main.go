8d5f7c4173786dbb7dbf30899248399cfb055f25
DebdipWritesCode <debdipmukherjee52@gmail.com>
2025-03-16 14:26:15 +0530

    fix minor issue

--- app/main.go
+++ app/main.go
@@ -479,8 +479,8 @@ func main() {
 					fmt.Print("\a") // Beep sound suggesting no suggestions
 				} else if len(suggestions) == 1 {
 					tab_pressed = false
-					input_buffer = []byte(suggestions[0]) // Auto-complete with the only match
-					fmt.Print("\r\x1b[K")                 // Clears the line
+					input_buffer = []byte(suggestions[0] + " ") // Auto-complete with the only match
+					fmt.Print("\r\x1b[K")                       // Clears the line
 					fmt.Printf("$ %s", input_buffer)
 				} else if len(suggestions) > 1 {
 					if tab_pressed {
